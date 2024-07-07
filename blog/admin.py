399da83f8512100b7a8e61b69c8849c64e137f07
ismaily1 <zzuurr123@icloud.com>
2024-07-07 18:23:57 +0300

    searchfield

--- blog/admin.py
+++ blog/admin.py
@@ -5,8 +5,9 @@ from .models import blog,category
 class BlogAdmin(admin.ModelAdmin):
     list_display = ("title","is_active","is_home","slug",)
     list_editable = ("is_active","is_home",)
-    search_fields = ("title", "description",)
+    search_fields = ("title", "description","slug")
     readonly_fields = ("description",)
+    
 
 
 admin.site.register(blog, BlogAdmin)
